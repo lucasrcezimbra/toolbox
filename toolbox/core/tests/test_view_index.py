0b03de8d4fb9701722f4ccaa68f3b637efc2991d
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-10-01 20:54:08 -0300

    test: fix

--- toolbox/core/tests/test_view_index.py
+++ toolbox/core/tests/test_view_index.py
@@ -1,14 +1,17 @@
 from http import HTTPStatus
 
+import pytest
 from django.shortcuts import resolve_url
 from pytest_django.asserts import assertTemplateUsed
 
 
+@pytest.mark.django_db
 def test_status_code(client):
     response = client.get(resolve_url("index"))
     assert response.status_code == HTTPStatus.OK
 
 
+@pytest.mark.django_db
 def test_template(client):
     response = client.get(resolve_url("index"))
     assertTemplateUsed(response, "index.html")
