10b0448e673afb5d54dff26a565819c7bf17fec4
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-10-03 06:51:34 -0300

    feat(index): add number of tags

--- toolbox/core/views.py
+++ toolbox/core/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from taggit.models import Tag
 
 from toolbox.core.models import Tool
 
 
 def index(request):
+    number_of_tools = Tool.objects.count()
+    number_of_tags = Tag.objects.count()
     tools = Tool.objects.order_by("-created_at")[:20]
-    total = Tool.objects.count()
-    return render(request, "index.html", {"tools": tools, "total": total})
+    return render(
+        request,
+        "index.html",
+        {
+            "number_of_tags": number_of_tags,
+            "number_of_tools": number_of_tools,
+            "tools": tools,
+        },
+    )
