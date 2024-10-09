362eaf82b1bff69adea890fda5d4be5a76b36c52
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-10-09 07:43:01 -0300

    fix: add get_by_natural_key

--- toolbox/core/models.py
+++ toolbox/core/models.py
@@ -2,27 +2,40 @@ from django.db import models
 from taggit.managers import TaggableManager
 
 
+class ToolManager(models.Manager):
+    def get_by_natural_key(self, url_github):
+        return self.get(url_github=url_github)
+
+
 class Tool(models.Model):
     added_at = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    url_github = models.URLField()
+    url_github = models.URLField(unique=True)
     name = models.CharField(max_length=254)
     notes = models.TextField(blank=True)
 
+    objects = ToolManager()
     tags = TaggableManager()
 
     def __str__(self):
         return self.name
 
     def natural_key(self):
-        return (self.name,)
+        return (self.url_github,)
+
+
+class ListManager(models.Manager):
+    def get_by_natural_key(self, slug):
+        return self.get(slug=slug)
 
 
 class List(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
     tools = models.ManyToManyField(Tool)
+
+    objects = ListManager()
 
     def __str__(self):
         return self.name
