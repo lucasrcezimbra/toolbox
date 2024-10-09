0b175dba7219bc9003d21f83c31e0224721db675
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-10-09 07:25:46 -0300

    fix: distill for list_detail url

--- toolbox/urls.py
+++ toolbox/urls.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from django.urls import path
 from django_distill import distill_path
 
+from toolbox.core.models import List
 from toolbox.core.views import index, list_detail, lists, tags
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     distill_path("", index, name="index"),
     distill_path("lists/", lists, name="lists"),
-    distill_path("lists/<str:slug>/", list_detail, name="list-detail"),
+    distill_path(
+        "lists/<str:slug>/",
+        list_detail,
+        name="list-detail",
+        distill_func=lambda: (x.slug for x in List.objects.all()),
+    ),
     distill_path("tags/", tags, name="tags"),
 ]
