5657016cc2c368a0e220cde1d1af71a0f330c167
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-10-09 07:07:53 -0300

    feat: List and Tool relation

--- toolbox/core/admin.py
+++ toolbox/core/admin.py
@@ -5,7 +5,10 @@ from toolbox.core.models import List, Tool
 
 @admin.register(List)
 class ListAdmin(admin.ModelAdmin):
+    autocomplete_fields = ("tools",)
     prepopulated_fields = {"slug": ("name",)}
 
 
-admin.site.register(Tool)
+@admin.register(Tool)
+class ToolAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
