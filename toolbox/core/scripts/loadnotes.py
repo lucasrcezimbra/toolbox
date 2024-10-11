2c283c8d3280613dc719e06af7628e73770e2d43
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-10-11 19:15:12 -0300

    chore: debug loadnotes

--- toolbox/core/scripts/loadnotes.py
+++ toolbox/core/scripts/loadnotes.py
@@ -4,4 +4,5 @@ from django.core.serializers import deserialize
 def run():
     with open("data/partial_tools_notes.json", "r") as f:
         for deserialized in deserialize("json", f):
+            print("[DEBUG]", deserialized.object.url_github)
             deserialized.object.save(update_fields=["notes"])
