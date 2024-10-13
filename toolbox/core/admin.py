dc3d24438cc1f243c248bd86934c95dea0dc6ee3
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2024-10-13 16:55:51 -0300

    feat(admin): prepopulate Tool.slug

--- toolbox/core/admin.py
+++ toolbox/core/admin.py
@@ -13,4 +13,5 @@ class ListAdmin(admin.ModelAdmin):
 class ToolAdmin(admin.ModelAdmin):
     list_display = ("name", "added_at", "url_github", "url_docs", "notes")
     ordering = ["-added_at"]
+    prepopulated_fields = {"slug": ("name",)}
     search_fields = ("name",)
